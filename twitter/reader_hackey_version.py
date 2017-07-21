2678d2e982c963fcf31fecc44839f4ccd27f3b57
Daan Goumans <daan.goumans@hotmail.com>
2017-07-21 23:32:30 +0200

    Changes to be committed: 	modified:   twitter/reader_hackey_version.py 	renamed:    twitter/server/index.php -> twitter/server/get.php

--- twitter/reader_hackey_version.py
+++ twitter/reader_hackey_version.py
@@ -4,43 +4,51 @@ import gc
 import time
 import requests
 
-def get_tweets(pushed,number):
-    if(pushed):
-        gc.collect()
-        try:
-            data = requests.get("https://example.com/sha2017/index.php")
-        except:
-            print("Could not download JSON!")
-            time.sleep(1)
-            return
-        try:
-            global tweets
-            tweets = data.json()
-        except:
-            data.close()
-            print("Could not decode JSON!")
-            time.sleep(1)
-            return
-        data.close()
+def get_tweets(hashtag):
+	gc.collect()
+	try:
+		data = requests.get("http://192.168.1.147/html/twitter/get.php?hashtag="+hashtag)
+	except:
+		print("Could not download JSON!")
+		time.sleep(1)
+		return
+	try:
+		global tweets
+		tweets = data.json()
+	except:
+		data.close()
+		print("Could not decode JSON!")
+		time.sleep(1)
+		return
+	
+	data.close()
+	x=0
+	tweet_array = []
+	amount =  len(tweets['statuses'])
+	if amount >= 1:
+		for x in range(0,amount):
+			tweet_user = "@" + str(tweets['statuses'][x]['user']['screen_name'].encode("utf-8"))
+			tweet_text = str(tweets['statuses'][x]['text'].encode("utf-8"))
+			tweet_array.append(str(tweet_user+"\r\n"+tweet_text))
+		return tweet_array
+	else:
+		tweet_array.append("try another hashtag")
+		return tweet_array
 
-    x=0
-    tweet_array = []
-    for x in range(0, 20):
-        tweet_user = "@" + str(tweets['statuses'][x]['user']['screen_name'].encode("utf-8"))
-        tweet_text = str(tweets['statuses'][x]['text'].encode("utf-8"))
-        tweet_array.append(str(tweet_user+"\r\n"+tweet_text))
-    return tweet_array[number]
+def loop_through_tweets(array_of_tweets):
+	for index, tweet in enumerate(array_of_tweets):
+		#print "-----------------"
+		#print index
+		#print "-----"
+		print tweet
+		time.sleep(0.1)
 
-def main(number):
-    if(number.isdigit()):
-        tweet_number = int(number)
-        if 0 <= tweet_number <= 19:
-            print(get_tweets(1,tweet_number))
-        else:
-            print "Please insert a number between 0 and 19"
+def main(hashtag):
+	tweets = get_tweets(hashtag)
+	loop_through_tweets(tweets)
 
-if len(sys.argv) < 2:
-    print "Usage: python reader.py #0-19"
+if len(sys.argv) > 1:
+	hashtag = sys.argv[1]
+	main(hashtag)
 else:
-    number = sys.argv[1]
-    main(number)
+	main('')
--- twitter/server/get.php
+++ twitter/server/get.php
@@ -1,22 +1,31 @@
-<?
+<?php
 //We use already made Twitter OAuth library
 //https://github.com/mynetx/codebird-php
 require_once ('codebird.php');
 
 //Twitter OAuth Settings
-$CONSUMER_KEY = '...';
-$CONSUMER_SECRET = '...';
-$ACCESS_TOKEN = '...';
-$ACCESS_TOKEN_SECRET = '...';
+$CONSUMER_KEY = '';
+$CONSUMER_SECRET = '';
+$ACCESS_TOKEN = '';
+$ACCESS_TOKEN_SECRET = '';
 
 //Get authenticated
 Codebird\Codebird::setConsumerKey($CONSUMER_KEY, $CONSUMER_SECRET);
 $cb = Codebird\Codebird::getInstance();
 $cb->setToken($ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
 
+//retrieve parameters
+//https://dev.twitter.com/rest/reference/get/search/tweets
+if (isset($_GET['hashtag'])) {
+        $hashtag = $_GET['hashtag'];
+    }else{
+        // Fallback behaviour goes here
+		$hashtag = 'sha2017';
+    }
+
 //retrieve posts
-$q = '%23Sha2017';
-$count = '2';
+$q = '%23'.$hashtag;
+$count = 100;
 $api = 'search_tweets';
 
 //https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
@@ -24,7 +33,7 @@ $api = 'search_tweets';
 $params = array(
 'screen_name' => $q,
 'q' => $q,
-'count' => $count
+'count' => $count,
 );
 
 //Make the REST call
